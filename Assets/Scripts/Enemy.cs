5b7dfc1489107594ad2adab5700aa62b92fcdc12
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-09-02 21:32:15 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public abstract  class Enemy : MonoBehaviour
 {
-    [SerializeField] protected GameObject destroyEffect;
-    [SerializeField] protected float destroyTime;
     protected void OnCollisionEnter2D(Collision2D other)
     {
         Vector2 contactPoint = other.GetContact(0).point;
