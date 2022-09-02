2c1958782fb142ef6e664601578a100e77af5ad3
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-09-02 20:46:45 +0300

    enemy destroy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] protected GameObject destroyEffect;
+    [SerializeField] private float destroyTime;
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Vector2 contactPoint = other.GetContact(0).point;
+
         GameObject gameObject = other.gameObject;
 
         if (gameObject != null)
         {
             if (gameObject.TryGetComponent(out BulletMovement _))
             {
-                Debug.Log("dest");
                 Destroy(this.gameObject);
+                CreateBulletDestroyEffect(contactPoint);
             }
         }
+    }
+    public void CreateBulletDestroyEffect(Vector2 contactPoint)
+    {
+        GameObject effect = Instantiate(destroyEffect, contactPoint, Quaternion.identity);
+        Destroy(destroyEffect, destroyTime);
     }
 }
