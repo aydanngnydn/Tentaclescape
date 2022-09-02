37eacc124517d1221a0127d942318d5251261f45
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-09-02 22:44:42 +0300

    circ num

--- Assets/Scripts/CircularEnemy.cs
+++ Assets/Scripts/CircularEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,32 @@ public class CircularEnemy : MonoBehaviour
 {
     public float angularSpeed = 1f;
     public float circleRad = 1f;
- 
+
     private float currentAngle;
 
     [SerializeField] private float xSpeed;
 
-    void Update ()
+    private Vector3 pos;
+
+    private void Start()
     {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+
         currentAngle += angularSpeed * Time.deltaTime;
-        Vector2 offset = new Vector2 (Mathf.Sin (currentAngle), Mathf.Cos (currentAngle)) * circleRad;
-        transform.Translate( (offset + new Vector2(xSpeed, 0)) * Time.deltaTime);
+        Vector2 offset = new Vector2(Mathf.Sin(currentAngle), Mathf.Cos(currentAngle)) * circleRad;
+
+        if (pos.x > 0)
+        {
+            transform.Translate(-(offset + new Vector2(xSpeed, 0)) * Time.deltaTime);
+        }
+        else if (pos.x < 0)
+        {
+            transform.Translate((offset + new Vector2(xSpeed, 0)) * Time.deltaTime);
+
+        }
     }
 }
