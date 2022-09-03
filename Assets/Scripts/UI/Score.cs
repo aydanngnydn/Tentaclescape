e9604f8a650ea762ca89a805a08b764541c7dd7d
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-09-03 17:06:05 +0300

    push

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,44 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private float scorePerSecond;
+
+    private float score;
+    private float lastScore;
+    
+    void Start()
+    {
+        score = 0;
+        lastScore = 0;
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            ScorePanel();
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            ScoreEndPanel();
+        }
+    }
+    
+    private void ScorePanel()
+    {
+        scoreText.text = ((int)score).ToString();
+        score += scorePerSecond * Time.deltaTime;
+        
+    }
+    private void ScoreEndPanel()
+    {
+        scoreText.text = "Score: " + ((int)score).ToString();
+        score += scorePerSecond * Time.deltaTime;
+    }
+}
+
