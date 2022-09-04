34fa60596d2e316bf496f34adcd46169f87756ed
karataskn20 <karataskn20@gmail.com>
2022-09-04 19:51:23 +0300

    Update enemy collider

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public abstract class Enemy : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private Collider2D collider;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        collider = GetComponent<Collider2D>();
+    }
+
     protected void OnCollisionEnter2D(Collision2D other)
     {
         GameObject gameObject = other.gameObject;
@@ -14,6 +22,7 @@ public abstract class Enemy : MonoBehaviour
             if (gameObject.TryGetComponent(out BulletMovement _))
             {
                 anim.SetTrigger("Death");
+                Destroy(collider);
                 Destroy (this.gameObject, anim.runtimeAnimatorController.animationClips.Length); 
             }
         }
